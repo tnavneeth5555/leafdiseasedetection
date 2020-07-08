b8dfc6410e6966cd8c8411d58d7645a12878beeb
sai70977venu <tnavneeth5555@gmail.com>
2020-07-08 16:49:38 +0530

    commit-1

---
+++ app.py
@@ -0,0 +1,95 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+import pickle
+# Keras
+
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+import keras
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+import tensorflow as tf
+# Define a flask app
+app = Flask(__name__)
+session = keras.backend.get_session()
+init = tf.global_variables_initializer()
+session.run(init)
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/cnn_model.pkl'
+
+# Load your trained model
+model = pickle.load(open(MODEL_PATH, 'rb'))
+model._make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+graph=tf.get_default_graph()
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+
+    img = image.load_img(img_path, target_size=(256,256,3))
+    
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    x.reshape(1,256,256,3)
+
+    print(x)
+    x = np.expand_dims(x, axis=0)
+    prob =model.predict(x)
+    print(prob)
+    return prob
+    
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+   
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        global graph
+        with graph.as_default():
+            f = request.files['file']
+
+            # Save the file to ./uploads
+            basepath = os.path.dirname(__file__)
+            file_path = os.path.join(
+                basepath, 'uploads', secure_filename(f.filename))
+            f.save(file_path)
+
+            # Make prediction
+            preds = model_predict(file_path, model)
+            print(preds)
+            # Process your result for human
+            # pred_class = preds.argmax(axis=-1)            # Simple argmax
+            pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+            result = str(pred_class[0][0][1])               # Convert to string
+            return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
